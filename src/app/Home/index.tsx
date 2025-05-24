5d2f067e9f0868d5a74950725629fe772dc9dab1
rockske8er <rockske8er@gmail.com>
2025-05-24 14:07:32 -0300

    feat: create a layout for home screen app and mofify aliases path

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,35 @@
+
+import { Image, TouchableOpacity, View, Text } from "react-native"
+
+import { styles } from "./styles"
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
+import { Filter } from "@/components/Filter"
+import { FilterStatus } from "@/types/filter-status"
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
+      
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}> 
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+          
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
+}
